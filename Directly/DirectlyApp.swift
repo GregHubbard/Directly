6f320e17beecdee6aa6c0189d1642acce16bb29d
GregHubbard <gdhubbardjr@gmail.com>
2023-02-09 16:55:59 -0500

    Initial Commit

---
+++ Directly/DirectlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirectlyApp.swift
+//  Directly
+//
+//  Created by Greg Hubbard on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct DirectlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
